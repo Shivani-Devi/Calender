f71aac0e6504580fd62e4025a5a2f1ffe058b8b9
Shivani-Devi <shivanijaryal884@gmail.com>
2024-06-10 21:19:33 -0400

    Created calendr and time picker dialog box

--- app/src/main/java/algonquin/cst2335/lab03/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/lab03/MainActivity.java
@@ -1,6 +1,18 @@
 package algonquin.cst2335.lab03;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +21,45 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtView;
+     TextView txtView2;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+      txtView=(TextView) findViewById(R.id.txtView);
+        txtView2=(TextView) findViewById(R.id.txtView2);
+        txtView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        txtView.setText((month+1)+"/"+dayOfMonth+"/"+year);
+                    }
+                },0,0,0);
+                datePickerDialog.show();
+            }
+
         });
+
+       txtView2.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               TimePickerDialog timePickerDialog=new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                   @Override
+                   public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                       txtView2.setText(hourOfDay+":"+minute);
+                   }
+               },0,0,true);
+               timePickerDialog.show();
+
+           }
+       });
+
     }
-}
+
+    }
+
